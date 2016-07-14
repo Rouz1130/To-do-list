646cd0be716f8d1e3230e2669772da0e9db29acc
rouz majlessi and russ vetsper <rouz+russ@>
2016-07-13 17:27:25 -0700

    fix startup.cs

--- startup.cs
+++ startup.cs
@@ -1,32 +1,42 @@
+using System.Collections.Generic;
 using System.IO;
 using Microsoft.AspNet.Builder;
 using Nancy;
-using Nancy.ViewEngines.Razor;
-using System.Collections.Generic;
+using Nancy.Owin;
 
-namespace ToDoList
+using Nancy.ViewEngines.Razor;
+
+namespace FriendLetter
 {
   public class Startup
   {
     public void Configure(IApplicationBuilder app)
     {
       app.UseOwin(x => x.UseNancy());
-     }
+    }
   }
-   public class CustomRootPathProvider :  IRootPathProvider
-   {
-     public string GetRootPath()
-     {
-       return Directory.GetCurrentDirectory();
-     }
-   }
-   public IEnumberable<string>GetDefaultNamespaces()
-   {
-     return null;
-   }
-   public bool AutoIncludeModelNamespace
-   {
-     get {return false;}
-   }
+  public class CustomRootPathProvider : IRootPathProvider
+  {
+    public string GetRootPath()
+    {
+      return Directory.GetCurrentDirectory();
+    }
+  }
+  public class RazorConfig : IRazorConfiguration
+  {
+    public IEnumerable<string> GetAssemblyNames()
+    {
+      return null;
+    }
+
+    public IEnumerable<string> GetDefaultNamespaces()
+    {
+      return null;
+    }
+
+    public bool AutoIncludeModelNamespace
+    {
+      get { return false; }
+    }
   }
 }
